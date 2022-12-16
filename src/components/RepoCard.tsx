2c5709129aff9bd98faa23596844ed9c9ac3e4d5
Timur Tarazov <johnsyplay@yandex.ru>
2022-12-16 22:36:30 +0500

    project is done

--- src/components/RepoCard.tsx
+++ src/components/RepoCard.tsx
@@ -1,16 +1,44 @@
+import React, { useState } from "react";
 import { IRepo } from "../models/models";
+import { useActions } from "../hooks/actions";
+import { useAppSelector } from "../hooks/redux";
 
 export default function RepoCard({repo}: {repo: IRepo}) {
+
+    const {addFavourite, removeFavourite} = useActions();
+    const {favourites} = useAppSelector(state => state.github);
+    const [isFav, setFav] = useState(favourites.includes(repo.html_url));
+
+    const addToFavs = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+        addFavourite(repo.html_url);
+        setFav(true);
+    };
+    const removeFavs = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+        removeFavourite(repo.html_url);
+        setFav(false);
+    };
+
     return (
-        <a href={repo.html_url} target="_blank">
-            <div className="repo-card border py-3 px-2 rounded mb-2 hover:shadow-md hover:bg-gray-100 transition-all">
+        <div className="repo-card border py-3 px-2 rounded mb-2 hover:shadow-md hover:bg-gray-100 transition-all">
+            <a href={repo.html_url} target="_blank">
                 <h2 className="font-bold text-lh">{repo.full_name}</h2>
                 <p className="text-small description">
                     Forks: <span className="font-bold">{repo.forks}</span>
                     Watchers: <span className="font-bold">{repo.watchers}</span>
                     {repo.description && <span>Description: {repo.description}</span>}
                 </p>
-            </div>
-        </a>
+            </a>
+            {isFav ? <button
+                className="py-2 px-4 bg-red-400 rounded hover:shadow-md transition-all"
+                onClick={removeFavs}
+            >Remove from favs</button> : 
+            <button
+            className="py-2 px-4 mr-2 bg-yellow-400 rounded hover:shadow-md transition-all"
+            onClick={addToFavs}
+            >Add to favs</button>}
+
+        </div>
     );
 }
--- src/pages/Favourites.tsx
+++ src/pages/Favourites.tsx
@@ -1,7 +1,23 @@
+import { useAppSelector } from "../hooks/redux";
+
 export default function Favourites() {
-    return (
-        <div className="favourites">
-            <h1>Favourites</h1>
-        </div>
-    );
+
+    const {favourites} = useAppSelector(state => state.github);
+
+    if (favourites.length === 0) {
+        return <p>No items were added</p>
+    } else {
+        return (
+            <div className="favourites flex justify-center mx-auto h-screen">
+                <ul className="list-none">
+                    {favourites.map(el => (
+                        <li key={el}>
+                            <a href={el} target="_blank">{el}</a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        );
+    }
+
 }
